4f8229ab75e1beb812c3316618d95940aa1daa6f
Bhavya Kashyap <47695886+bhavya-kashyap@users.noreply.github.com>
2020-09-08 01:45:30 +0530

    Update otp.py

--- application/otp.py
+++ application/otp.py
@@ -7,7 +7,7 @@ def generate_otp(phone):
     t = totp.now()
     phoneno = str('+91')+str(phone)
     # the following line needs your Twilio Account SID and Auth Token
-    client = Client("AC8e7ebb0968e6be74705ba77e4d93c62d", "728a61d603ca18e538711353170d0117")
+    client = Client("AC8e7ebb0968e6be74705ba77e4d93c62d", "2c86241cfeb729ca144125287044ac0e")
     # change the "from_" number to your Twilio number and the "to" number
     # to the phone number you signed up for Twilio with, or upgrade your
     # account to send SMS to any phone number
